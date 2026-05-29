8aa5b68ad7a3166263a416ea24e147be183e1489
Renan Oliveira <renanlido@yahoo.com.br>
2026-05-29 02:58:45 -0300

    refactor: LaneController routes event type to use cases

--- src/LaneController.ts
+++ src/LaneController.ts
@@ -1,12 +1,33 @@
-import { LaneRegistry } from "./domain/LaneRegistry.js";
 import type { FlowEvent } from "./flow/events.js";
+import { StartOperation } from "./application/use-cases/StartOperation.js";
+import { CorrectPlate } from "./application/use-cases/CorrectPlate.js";
+import { ApproveRelease } from "./application/use-cases/ApproveRelease.js";
+import { CancelOperation } from "./application/use-cases/CancelOperation.js";
+import { ResetLane } from "./application/use-cases/ResetLane.js";
+import { IngestLaneSignal } from "./application/use-cases/IngestLaneSignal.js";
 
 export class LaneController {
+  private readonly startOperation = new StartOperation();
+  private readonly correctPlate = new CorrectPlate();
+  private readonly approveRelease = new ApproveRelease();
+  private readonly cancelOperation = new CancelOperation();
+  private readonly resetLane = new ResetLane();
+  private readonly ingestSignal = new IngestLaneSignal();
+
   async command(laneId: string, ev: FlowEvent): Promise<void> {
-    const lane = LaneRegistry.peek(laneId);
-    if (!lane) {
-      throw new Error(`lane not found: ${laneId}`);
+    switch (ev.type) {
+      case "startOperation":
+        return this.startOperation.execute(laneId, ev.side);
+      case "correctPlate":
+        return this.correctPlate.execute(laneId, ev.value);
+      case "operatorApprove":
+        return this.approveRelease.execute(laneId);
+      case "operatorCancel":
+        return this.cancelOperation.execute(laneId);
+      case "manualReset":
+        return this.resetLane.execute(laneId);
+      default:
+        return this.ingestSignal.execute(laneId, ev);
     }
-    await lane.send(ev);
   }
 }
