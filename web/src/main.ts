31bd944e3aa97c45b4f4ec945760a7c2ed95e0e9
Renan Oliveira <renanlido@yahoo.com.br>
2026-05-29 01:59:45 -0300

    feat: front render (scene, panels, timeline, controls, bootstrap)

---
+++ web/src/main.ts
@@ -0,0 +1,34 @@
+import { initialState, reduce, type UiState } from "./state.js";
+import { openStream, getSnapshot } from "./api.js";
+import { Scene } from "./scene.js";
+import { renderBadge, renderSensors, renderIntegrations } from "./panels.js";
+import { renderTimeline } from "./timeline.js";
+import { renderControls } from "./controls.js";
+
+const $ = (id: string) => document.getElementById(id) as HTMLElement;
+
+let state: UiState = initialState();
+const scene = new Scene($("stage"));
+
+function render(): void {
+  renderBadge($("stateBadge"), $("opId"), state);
+  renderSensors($("sensors"), state);
+  renderIntegrations($("integrations"), state);
+  renderTimeline($("timeline"), state);
+}
+
+renderControls($("controls"));
+render();
+
+getSnapshot()
+  .then((snap) => {
+    state = reduce(state, { topic: "lane.state", payload: snap, ts: Date.now() });
+    render();
+  })
+  .catch(() => undefined);
+
+openStream((msg) => {
+  state = reduce(state, msg);
+  scene.apply(msg);
+  render();
+});
